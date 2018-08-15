89c6caeade05b185035761b12944d9ea50164aef
Peijo <peijo97@gmail.com>
2018-08-14 22:13:23 -0700

    Lambda function now checks if user exists
    Lambda now returns status code 200 if the table does not contain duplicates and returns status code 400 if such a user exists with the same information.

--- lambda_functions/register_new_user.py
+++ lambda_functions/register_new_user.py
@@ -1,9 +1,17 @@
 import boto3
 import json
 
+'''
+This lambda takes a json describing the user and checks if a duplicate one already exists based on the email.
+It returns status code 200 if the table does not contain duplicates and returns status code 400 if such a user exists with the same information.
+'''
+
 def lambda_handler(event, context):
     # Given user data save in dynamoDB
     client = boto3.client('dynamodb')
+    response = client.get_item(TableName='Technica-Data',Key={'email':{'S':event['email']}})
+    if 'Item' in response:
+        raise Exception('Invalid input: user already exists.')
     response = client.put_item(TableName='Technica-Data',Item={
         'email':{'S':event['email']},
         'first_name':{'S':event['first_name']},
